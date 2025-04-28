fd033033d383509fe78339814ffd4032572048d8
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-04-28 22:31:47 +0300

    third commit

--- stock_app/stocks/views.py
+++ stock_app/stocks/views.py
@@ -6,7 +6,6 @@ def dashboard(request):
     companies = Company.objects.all()
     return render(request, 'stocks/dashboard.html', {'companies': companies})
 
-
 def stock_chart(request, ticker):
     # No need to unquote since path converter handles it
     company = get_object_or_404(Company, ticker=ticker)
